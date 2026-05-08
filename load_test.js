90077727bc175f7390bade6b9ad466b236ff1505
Sabbir Ahmed Rony <160509348+Sabbir-Rony@users.noreply.github.com>
2026-05-08 23:27:40 +0600

    Update load_test.js

--- load_test.js
+++ load_test.js
@@ -1,7 +1,12 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  vus: 10, // ১০ জন ভার্চুয়াল ইউজার
+  duration: '10s', // ১০ সেকেন্ড ধরে টেস্ট চলবে
+};
+
 export default function () {
-  http.get('https://google.com'); // এখানে আপনার আসল ডোমেইন দিতে পারেন
+  http.get('https://google.com'); // এখানে আপনার আসল API বা ওয়েবসাইটের লিঙ্ক দিন
   sleep(1);
 }
