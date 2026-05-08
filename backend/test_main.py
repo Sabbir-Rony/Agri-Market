a7327771b5e7f0d8cbc84874aa3a0eb1f04af378
Sabbir Ahmed Rony <160509348+Sabbir-Rony@users.noreply.github.com>
2026-05-08 23:05:29 +0600

    Create test_main.py

---
+++ backend/test_main.py
@@ -0,0 +1,8 @@
+# backend/test_main.py
+def test_check_math():
+    # এটি একটি সাধারণ টেস্ট: ১+১ সমান ২ কি না তা চেক করবে
+    assert 1 + 1 == 2
+
+def test_api_status():
+    # ভবিষ্যতে এখানে আপনি আপনার API চেক করার কোড লিখবেন
+    pass
